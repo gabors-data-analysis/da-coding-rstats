c9cd52685f3b75d47fa56d6596ac663b1f7613f6
Agoston Reguly <regulyagoston@gmail.com>
2022-03-24 11:46:51 +0100

    assignment01 added

---
+++ lecture01-intro/assignment_1.R
@@ -0,0 +1,38 @@
+#########################
+## Assignment 1        ##
+##    Lecture 1        ##
+##                     ##
+##  Deadline:          ##
+##                     ##
+#########################
+
+##
+# Task: fill out the blank parts with the needed commands
+
+# 1) Add the command which clears the environment
+
+
+# 2) Create a string variable, which states: 'This is my first assignment in R!'
+str_var <- 
+
+# 3) Decide with the proper command whether this is truly a string variable or not:
+
+  
+# 4) Create vector 'v', which contains the values of: 3, 363, 777, 2021, -987 and Inf
+v <- 
+  
+# 5) Multiply this vector with 10 and name it as v_10
+  
+
+# 6) Create a list, which contains 'str_var' variable and 'v' vector
+mL <-
+  
+# 7) Get the value of 'Inf' out of this 'mL' variable with indexing
+  
+
+# +1) decide whether the former extracted value is infinite or not. 
+#   The result should be a logical value.
+  
+  
+  
+  
