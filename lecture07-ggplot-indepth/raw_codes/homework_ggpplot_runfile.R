6b9db0d9f7f932124839db5a566d4abeec58ce4d
Agoston Reguly <regulyagoston@gmail.com>
2022-04-01 11:56:56 +0200

    homework

---
+++ lecture07-ggplot-indepth/raw_codes/homework_ggpplot_runfile.R
@@ -0,0 +1,27 @@
+#########################
+## Homework for        ##
+##    ggplot in-depth  ##
+##  Deadline:          ##
+##                     ##
+#########################
+
+##
+# Create your own theme for ggplot!
+#   
+# 0) Clear your environment
+
+# 1) Load tidyverse 
+
+# 2) use the same dataset as in class:
+df <- read_csv( "https://raw.githubusercontent.com/gabors-data-analysis/da-coding-rstats/main/lecture07-ggplot-indepth/data/hotels-vienna-london.csv" )
+
+# 3) Call your personalized ggplot function
+
+# 4) Run the following piece of command:
+ggplot( filter( df , city == 'Vienna' ) , aes( x = price ) ) +
+  geom_histogram( alpha = 0.8, binwidth = 20 ) +
+  labs(x='Hotel Prices in  Vienna',y='Density')+
+  theme_YOURFUNCTIONNAME()
+
+
+
---
+++ lecture07-ggplot-indepth/raw_codes/theme_RENAMEME.R
@@ -0,0 +1,25 @@
+#########################
+## Assignment 4        ##
+##      CLASS 4        ##
+##  Deadline:          ##
+##  2021/11/2 17:40   ##
+#########################
+
+##
+# Create your own theme for ggplot!
+#   In principle you should use this ggplot theme in the remainder of the course for assignments ect.
+#   Of course you can change along, but I would like to encourage all of you to use a personalized theme!
+#
+#  !! Please RENAME this file and call it accordingly in the runfile !! 
+#
+#   To get 7 points you will need to modify at least 7 parameters of the theme_classic or theme_bw!
+#
+#  Useful resources you may want to check:
+#     https://www.datanovia.com/en/blog/ggplot-themes-gallery/
+#  Or the book's theme:
+#       https://github.com/gabors-data-analysis/da_case_studies/blob/master/ch00-tech-prep/theme_bg.R
+#  Some more advanced/elaborated examples:
+#     https://bookdown.org/rdpeng/RProgDA/building-a-new-theme.html
+#     https://towardsdatascience.com/5-steps-for-creating-your-own-ggplot-theme-656e79a96b9
+#
+# and many more....
