ed49d83a57345f83989bb4d8e2548c33295d883c
Ágoston Reguly <40354586+regulyagoston@users.noreply.github.com>
2022-05-25 10:35:39 +0200

    font size for axistext

--- lecture07-ggplot-indepth/raw_codes/theme_bluewhite.R
+++ lecture07-ggplot-indepth/raw_codes/theme_bluewhite.R
@@ -18,7 +18,7 @@ theme_bluewhite <- function( base_size = 11, base_family = "") {
       axis.line         = element_line(color = "red"),
       # Littel lines called ticks on the axis
       axis.ticks        = element_line(color = "steelblue"),
-      # Numbers on the axis
-      axis.text         = element_text(color = "navyblue")
+      # Color and font size for the numbers on the axis
+      axis.text         = element_text(color = "navyblue", font_size = 8)
     )
 }
